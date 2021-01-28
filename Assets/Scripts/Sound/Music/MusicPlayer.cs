0ca17c786e4494f200039d8ba89163ab476cf26c
Martijn Zandvliet <martijn@ramjetanvil.com>
2021-01-28 18:41:59 +0100

    Including a bunch of assets that were erroneously ignored in the initial release.

---
+++ Assets/Scripts/Sound/Music/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+using Fmod = FMODUnity.RuntimeManager;
+
+public class MusicPlayer : MonoBehaviour {
+    [SerializeField] private string _introMusic;
+    [SerializeField] private string[] _musicClips;
+
+    private System.Random _random;
+    private EventInstance _event;
+
+    void Awake() {
+        _random = new System.Random();
+    }
+
+    public void PlayIntro() {
+        _event = Fmod.CreateInstance(_introMusic);
+        _event.start();
+    }
+
+    public void PlayRandomMusic() {
+        var clip = _musicClips[_random.Next(_musicClips.Length)];
+        _event = Fmod.CreateInstance(clip);
+        _event.start();
+    }
+
+    public void StopAndFade() {
+        if (_event != null) {
+            _event.stop(STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+
+    private void OnDestroy() {
+        if (_event != null) {
+            _event.release();            
+        }
+    }
+}
